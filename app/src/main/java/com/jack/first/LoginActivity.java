5b68313e6c02f3562e355c906ed5c2fc229ee943
Jack <40079880+jacksrlt@users.noreply.github.com>
2021-10-21 10:10:55 +0200

    Added onClick intent to LOGIN button

--- app/src/main/java/com/jack/first/LoginActivity.java
+++ app/src/main/java/com/jack/first/LoginActivity.java
@@ -1,8 +1,9 @@
 package com.jack.first;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    //Method for LOGIN button
+    public void onClick(View v) {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
