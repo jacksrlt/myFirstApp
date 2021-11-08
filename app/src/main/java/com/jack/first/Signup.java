d772c0485e292ecba94e1a1ab9414d8d7cab611a
barrigamiguelm <barrigamiguelm@gmail.com>
2021-11-08 13:19:54 +0100

    Añadido activity para contraseña olvidada falta funcion click

--- app/src/main/java/com/jack/first/Signup.java
+++ app/src/main/java/com/jack/first/Signup.java
@@ -1,7 +1,10 @@
 package com.jack.first;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Signup extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+
+    public void openLogin(View v){
+        Intent intent = new Intent(Signup.this, LoginActivity.class);
+        startActivity(intent);
+    }
+    public void openMain(View v){
+        Intent intent = new Intent(Signup.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
